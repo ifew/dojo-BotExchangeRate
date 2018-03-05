e9888136cfec808a7560dbb4677be9eaf8e1bf64
iFew <chitpong@gmail.com>
2018-03-05 19:26:07 +0700

    Changed function name to call

--- tests/api.UnitTest/ExchangeRateServiceTest.cs
+++ tests/api.UnitTest/ExchangeRateServiceTest.cs
@@ -10,7 +10,7 @@ namespace api.UnitTest
         public void When_Exchange_Rate_1USD_To_31_5408000THB_Plus_Fee_Should_Be_32_33THB()
         {
             ExchangeRateService exchangeRate = new ExchangeRateService();
-            double actualResult = exchangeRate.CalculateAmountWithFee(31.5408000);
+            double actualResult = exchangeRate.ExchangeRate(1.00);
 
             Assert.Equal(32.33, actualResult);
         }
@@ -19,7 +19,7 @@ namespace api.UnitTest
         public void When_Exchange_Rate_100USD_To_3154_08000THB_Plus_Fee_Should_Be_3232_94THB()
         {
             ExchangeRateService exchangeRate = new ExchangeRateService();
-            double actualResult = exchangeRate.CalculateAmountWithFee(3154.08000);
+            double actualResult = exchangeRate.ExchangeRate(100.00);
 
             Assert.Equal(3232.94, actualResult);
         }
@@ -28,7 +28,7 @@ namespace api.UnitTest
         public void When_Exchange_Rate_101USD_To_3185_620800THB_Plus_Fee_Should_Be_3254_12THB()
         {
             ExchangeRateService exchangeRate = new ExchangeRateService();
-            double actualResult = exchangeRate.CalculateAmountWithFee(3185.620800);
+            double actualResult = exchangeRate.ExchangeRate(101.00);
 
             Assert.Equal(3254.12, actualResult);
         }
