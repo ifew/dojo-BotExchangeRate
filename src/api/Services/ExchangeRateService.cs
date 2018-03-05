fb20529b0d8e9b7f7194c02473698ed38d174d43
iFew <chitpong@gmail.com>
2018-03-05 19:26:42 +0700

    Added get fee function and refactoring to receive number for calculate

--- src/api/Services/ExchangeRateService.cs
+++ src/api/Services/ExchangeRateService.cs
@@ -4,16 +4,33 @@ namespace api.Services
 {
     public class ExchangeRateService
     {
-        public double CalculateAmountWithFee(double inputAmount)
+        public double ExchangeRate(double inputAmountUSD) 
         {
-            double amountWithFee = inputAmount + (inputAmount * 0.025);
+            double inputAmount = inputAmountUSD * 31.5408000;
+            double fee = _GetFee(inputAmountUSD);
+            return CalculateAmountWithFee(inputAmount, fee);
+        }
+        public double CalculateAmountWithFee(double inputAmount, double fee)
+        {
+            double amountWithFee = inputAmount + (inputAmount * fee);
 
             return _RoundCeiling2Digit(amountWithFee);
         }
 
-        private double _RoundCeiling2Digit(double inputAmount)
+        private double _GetFee(double inputAmount) 
         {
-            double result = Math.Round(Math.Ceiling(inputAmount * 100)/100, 2, MidpointRounding.AwayFromZero);
+            if(inputAmount <= 100) {
+                return 0.0250;
+            } else if(inputAmount > 100 && inputAmount <= 500) {
+                return 0.0215;
+            } else {
+                return 0.0200;
+            }
+        }
+
+        private double _RoundCeiling2Digit(double number)
+        {
+            double result = Math.Round(Math.Ceiling(number * 100)/100, 2, MidpointRounding.AwayFromZero);
 
             return result;
         }
