00c6d11834df0be0d1f7fc56db4a73828a9985fb
iFew <chitpong@gmail.com>
2018-03-05 19:34:27 +0700

    Added unit test for test case 499usd, 500usd, 501usd

--- tests/api.UnitTest/ExchangeRateServiceTest.cs
+++ tests/api.UnitTest/ExchangeRateServiceTest.cs
@@ -32,5 +32,32 @@ namespace api.UnitTest
 
             Assert.Equal(3254.12, actualResult);
         }
+
+        [Fact]
+        public void When_Exchange_Rate_499USD_To_15738_859200THB_Plus_Fee_Should_Be_16077_25THB()
+        {
+            ExchangeRateService exchangeRate = new ExchangeRateService();
+            double actualResult = exchangeRate.ExchangeRate(499.00);
+
+            Assert.Equal(16077.25, actualResult);
+        }
+
+        [Fact]
+        public void When_Exchange_Rate_500USD_To_15770_400000THB_Plus_Fee_Should_Be_16109_47THB()
+        {
+            ExchangeRateService exchangeRate = new ExchangeRateService();
+            double actualResult = exchangeRate.ExchangeRate(500.00);
+
+            Assert.Equal(16109.47, actualResult);
+        }
+
+        [Fact]
+        public void When_Exchange_Rate_501USD_To_15801_940800THB_Plus_Fee_Should_Be_16117_98THB()
+        {
+            ExchangeRateService exchangeRate = new ExchangeRateService();
+            double actualResult = exchangeRate.ExchangeRate(501.00);
+
+            Assert.Equal(16117.98, actualResult);
+        }
     }
 }
