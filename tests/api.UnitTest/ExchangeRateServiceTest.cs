18041971a1e10c05c4833f0e1ae4483a541e68d4
iFew <chitpong@gmail.com>
2018-03-05 19:01:27 +0700

    Added unit test When_Exchange_Rate_3154_08000_Plus_Fee_Should_Be_3232_94()

--- tests/api.UnitTest/ExchangeRateServiceTest.cs
+++ tests/api.UnitTest/ExchangeRateServiceTest.cs
@@ -14,5 +14,14 @@ namespace api.UnitTest
 
             Assert.Equal(32.33, actualResult);
         }
+
+        [Fact]
+        public void When_Exchange_Rate_3154_08000_Plus_Fee_Should_Be_3232_94()
+        {
+            ExchangeRateService exchangeRate = new ExchangeRateService();
+            double actualResult = exchangeRate.CalculateAmountWithFee(3154.08000);
+
+            Assert.Equal(3232.94, actualResult);
+        }
     }
 }
