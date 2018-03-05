95e23890770fe9571fef6af09d6f4a2a473b1112
iFew <chitpong@gmail.com>
2018-03-05 19:07:55 +0700

    Added unit test When_Exchange_Rate_101USD_To_3185_620800THB_Plus_Fee_Should_Be_3254_12THB()

--- tests/api.UnitTest/ExchangeRateServiceTest.cs
+++ tests/api.UnitTest/ExchangeRateServiceTest.cs
@@ -7,7 +7,7 @@ namespace api.UnitTest
     public class ExchangeRateServiceTest
     {
         [Fact]
-        public void When_Exchange_Rate_31_5408000_Plus_Fee_Should_Be_32_33()
+        public void When_Exchange_Rate_1USD_To_31_5408000THB_Plus_Fee_Should_Be_32_33THB()
         {
             ExchangeRateService exchangeRate = new ExchangeRateService();
             double actualResult = exchangeRate.CalculateAmountWithFee(31.5408000);
@@ -16,12 +16,21 @@ namespace api.UnitTest
         }
 
         [Fact]
-        public void When_Exchange_Rate_3154_08000_Plus_Fee_Should_Be_3232_94()
+        public void When_Exchange_Rate_100USD_To_3154_08000THB_Plus_Fee_Should_Be_3232_94THB()
         {
             ExchangeRateService exchangeRate = new ExchangeRateService();
             double actualResult = exchangeRate.CalculateAmountWithFee(3154.08000);
 
             Assert.Equal(3232.94, actualResult);
+        }
+
+        [Fact]
+        public void When_Exchange_Rate_101USD_To_3185_620800THB_Plus_Fee_Should_Be_3254_12THB()
+        {
+            ExchangeRateService exchangeRate = new ExchangeRateService();
+            double actualResult = exchangeRate.CalculateAmountWithFee(3185.620800);
+
+            Assert.Equal(3254.12, actualResult);
         }
     }
 }
